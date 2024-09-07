a992ded5d4dacc01486de52650718aa80596bfe4
Aman nema <71541659+aman007-123@users.noreply.github.com>
2024-09-07 20:00:55 +0530

    All other test cases

--- calculator.service.spec.ts
+++ calculator.service.spec.ts
@@ -17,4 +17,30 @@ describe('CalculatorService', () => {
   it('should return 0 for an empty string', () => {
     expect(service.Add('')).toEqual(0);
   });
+
+  it('should return the number itself for a single number string', () => {
+    expect(service.Add('1')).toEqual(1);
+  });
+
+  it('should return the sum of two numbers', () => {
+    expect(service.Add('1,2')).toEqual(3);
+  });
+
+  it('should handle an unknown amount of numbers', () => {
+    expect(service.Add('1,2,3,4,5')).toEqual(15);
+  });
+
+  it('should handle new lines between numbers', () => {
+    expect(service.Add('1\n2,3')).toEqual(6);
+  });
+
+  it('should support different delimiters', () => {
+    expect(service.Add('//;\n1;2')).toEqual(3);
+  });
+
+  it('should throw an exception for negative numbers', () => {
+    expect(() => service.Add('1,-2,3,-4')).toThrowError(
+      'Negatives not allowed: -2, -4'
+    );
+  });
 });
--- calculator.service.ts
+++ calculator.service.ts
@@ -4,9 +4,37 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class CalculatorService {
-  Add(numbers: string): any {
+  Add(numbers: string): number {
     if (numbers === '') {
       return 0;
     }
+
+    let delimiter = ',';
+    if (numbers.startsWith('//')) {
+      const delimiterLineEnd = numbers.indexOf('\n');
+      delimiter = numbers.substring(2, delimiterLineEnd);
+      numbers = numbers.substring(delimiterLineEnd + 1);
+    }
+
+    const regex = new RegExp('[\\n' + delimiter + ']');
+    const nums = numbers.split(regex);
+
+    let sum = 0;
+    const negatives: any = [];
+
+    nums.forEach((numStr) => {
+      const num = parseInt(numStr);
+      if (num < 0) {
+        negatives.push(num);
+      } else if (num <= 1000) {
+        // ignore numbers greater than 1000
+        sum += num;
+      }
+    });
+
+    if (negatives.length > 0) {
+      throw new Error('Negatives not allowed: ' + negatives.join(', '));
+    }
+    return sum;
   }
 }
